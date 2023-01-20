0f8fa1b312154ead18e8ad0e0577e77f57ad2693
kookyungseon <koo0685@naver.com>
2023-01-20 13:05:23 +0900

    allowedt update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!9a*l0ii%iwe0&v%!m&4o^-r!nnw6no(sqlr+$0j@(w!!*wa=q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-15-188-94-29.eu-west-3.compute.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-13-213-56-246.ap-southeast-1.compute.amazonaws.com','13.213.56.246']
 
 
 # Application definition
